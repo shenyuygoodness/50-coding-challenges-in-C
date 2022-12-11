36fb132ffd3ab9ba9234c79b2cb2ff94983abb20
shenyuygoodness <93448293+shenyuygoodness@users.noreply.github.com>
2022-12-11 23:48:52 +0100

    Recursive fibonacci

---
+++ ex15.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+int fib(int n);
+
+int fib(int n) {
+	if(n == 0){
+		return 0;
+	} else if(n == 1) {
+		return 1;
+	} else {
+		return (fib(n-1) + fib(n-2));
+	}
+}
+
+int main()
+{
+	int n = 4, i;
+	printf("fib for %d\n", n);
+	for(i = 0; i <=n; i++){
+		printf("%d\n", fib(i));
+	}
+}
+
+
